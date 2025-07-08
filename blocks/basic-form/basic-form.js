bab75e5b7fad328ae4377ac0e1ebc41839fb6d2b
Joanne Ho <joaho@adobe.com>
2025-07-08 14:39:24 -0700

    changed basic-form block from block to text to see if that will render.

--- blocks/basic-form/basic-form.js
+++ blocks/basic-form/basic-form.js
@@ -6,6 +6,7 @@ export default async function decorate(block) {
   // decorate footer DOM
   //block.textContent = '';
   const footer = document.createElement('div');
+  footer.classList.add('some-custom-here');
 
   block.append(footer);
   console.log('here');
